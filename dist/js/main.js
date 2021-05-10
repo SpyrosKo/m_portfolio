f3b978398697bfae7ead9cdc9959c9daced78834
SpyrosKo <spyros.konofaos1@gmail.com>
2021-05-10 14:11:43 -0400

    FEAT: Changing font families. Adding mobile menu.

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 let bars = document.querySelector('.menu-btn__burger');
 let header = document.querySelector('.header');
+let main = document.querySelector('.main');
 
 // Set initial state of menu
 let show = false;
@@ -12,13 +13,21 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
    if (!show) {
       menuBtn.classList.add('open');
-      header.style.marginRight = '400px';
+      header.style.marginRight = '450px';
       header.style.transition = 'all 1s';
+      header.opacity = '1';
+      main.style.opacity = '0.05';
+      main.style.backgroundColor = 'black';
+      main.style.color = 'black';
+
 
       show = true;
    } else {
       menuBtn.classList.remove('open');
-      header.style.marginRight = '-400px';
+      header.style.marginRight = '-450px';
+      main.style.opacity = '1';
+      main.style.backgroundColor = 'transparent';
+
       show = false;
    }
 
