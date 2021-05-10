359e66f674178dd7ffd1b80becd59fe7e3ccf027
SpyrosKo <spyros.konofaos1@gmail.com>
2021-05-10 16:40:22 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-// removing links
-let link = document.querySelector('.link');
+// DOM elements
+const menuBtn = document.querySelector('.menu-btn');
+let bars = document.querySelector('.menu-btn__burger');
+let header = document.querySelector('.header');
+let main = document.querySelector('.main');
 
-link.href = '';
+// Set initial state of menu
+let show = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+   if (!show) {
+      menuBtn.classList.add('open');
+      header.style.marginRight = '450px';
+      header.style.transition = 'all 1s';
+      header.opacity = '1';
+      main.style.opacity = '0.05';
+      main.style.backgroundColor = 'black';
+      main.style.color = 'black';
+
+
+      show = true;
+   } else {
+      menuBtn.classList.remove('open');
+      header.style.marginRight = '-450px';
+      main.style.opacity = '1';
+      main.style.backgroundColor = 'transparent';
+
+      show = false;
+   }
+
+}
+
+console.log(menuBtn);
